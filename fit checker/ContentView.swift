ff66774120240b1d9bbc6983d3775f9c5ccf711e
svenreyes <svenxx64@gmail.com>
2024-10-30 17:12:40 -0400

    Initial Commit

---
+++ fit checker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fit checker
+//
+//  Created by Sven Reyes on 10/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fit checker/fit_checkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fit_checkerApp.swift
+//  fit checker
+//
+//  Created by Sven Reyes on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct fit_checkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

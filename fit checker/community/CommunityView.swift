c00a3ddbdfc3ef9a439bfa9520d101cf65b32682
svenreyes <svenxx64@gmail.com>
2024-11-02 17:48:22 -0400

    community page

--- fit checker/community/CommunityView.swift
+++ fit checker/community/CommunityView.swift
@@ -1,20 +1,25 @@
-//
-//  CommunityView.swift
-//  fit checker
-//
-
 import SwiftUI
 
 struct CommunityView: View {
-    var isLoaded = true
+    let posts = [
+        Post(username: "user123", timestamp: "2 hours ago", fires: 45, rating: 87, imageName: "asap"),
+        Post(username: "fashionista", timestamp: "5 hours ago", fires: 30, rating: 92, imageName: "default"),
+        Post(username: "styleguru", timestamp: "1 day ago", fires: 60, rating: 75, imageName: "asap")
+    ]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Image(systemName: isLoaded ? "sun" : "photo")
-        
-        
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 16) {
+                    ForEach(posts) { post in
+                        PostView(post: post)
+                            .padding(.horizontal)
+                    }
+                }
+            }
+            .navigationTitle("Community")
+        }
     }
-    
 }
 
 #Preview {
---
+++ fit checker/community/UserProfileView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct UserProfileView: View {
+    let username: String
+    let posts: [Post]
+    
+    var body: some View {
+            ScrollView {
+                VStack(spacing: 16) {
+                    Image("default")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                        .padding(.top)
+
+                    Text("Outfits Posted")
+                        .font(.headline)
+                        .padding(.vertical)
+
+                    ForEach(posts) { post in
+                        PostView(post: post)
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .navigationTitle(username)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+
+// Move the sample data outside of the #Preview closure
+let samplePosts = [
+    Post(username: "user123", timestamp: "2 hours ago", fires: 45, rating: 87, imageName: "asap"),
+    Post(username: "user123", timestamp: "1 day ago", fires: 50, rating: 82, imageName: "default")
+]
+
+#Preview {
+    UserProfileView(username: "user123", posts: samplePosts)
+}

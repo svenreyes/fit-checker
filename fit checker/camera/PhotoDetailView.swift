2d13551d75fc6afb8e86b11a657308168bb52ee0
Advik Arora <144552518+advikarora@users.noreply.github.com>
2024-11-03 10:05:21 -0500

    Update PhotoDetailView.swift

--- fit checker/camera/PhotoDetailView.swift
+++ fit checker/camera/PhotoDetailView.swift
@@ -8,25 +8,15 @@ struct PhotoDetailView: View {
         VStack {
             Image(uiImage: photoItem.image)
                 .resizable()
-                .scaledToFit()
-                .frame(maxHeight: 300)
+                .scaledToFill()
+                .frame(height: 300)
+                .clipped() 
                 .cornerRadius(12)
                 .padding()
 
             Text("Rating: \(photoItem.rating)/10")
                 .font(.headline)
                 .padding(.bottom, 10)
-
-            Slider(value: Binding(
-                get: {
-                    Double(photoItem.rating)
-                },
-                set: { newValue in
-                    photoItem.rating = Int(newValue)
-                    photoCollection.updateRating(for: photoItem, rating: Int(newValue))
-                }
-            ), in: 0...10, step: 1)
-            .padding([.leading, .trailing], 20)
 
             Spacer()
         }
